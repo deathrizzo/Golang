b1dbb1ea14398b3afa7d38049fd4a83925f8a993
Methods <methods@hotmail.com>
2020-05-02 06:17:07 -0700

    fixing path

--- deployments/iam.tf
+++ deployments/iam.tf
@@ -20,5 +20,5 @@ resource "aws_iam_role" "example" {
   count              = length(var.user_names)
   name               = var.user_names[count.index]
   assume_role_policy = data.aws_iam_policy_document.instance-assume-role-policy.json
-  path               = "/orbis/*"
+  path               = "/orbis/"
 }
