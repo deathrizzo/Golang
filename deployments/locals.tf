91cd53b03fa5b2404043744d312707d0778a333c
Methods <methods@hotmail.com>
2020-05-13 11:14:01 -0700

    adding s3 bucket with tags

---
+++ deployments/locals.tf
@@ -0,0 +1,14 @@
+locals {
+  environment = "dev"
+  application = "orbis"
+  team = "dtc"
+  customer = "iStreamPlanet"
+  data-classification = "restricted"
+
+  common_tags = {
+      Environment = "${local.environment}"
+      Application = "${local.application}"
+      Team = "${local.team}"
+      Customer = "${local.customer}"
+  }
+}
---
+++ deployments/s3.tf
@@ -0,0 +1,12 @@
+resource "aws_s3_bucket" "example-dtc-bucket" {
+  bucket = "example-dtc-bucket"
+  acl = "private"
+
+  tags = {
+      Environment = "${local.environment}"
+      Application = "${local.application}"
+      Team = "${local.team}"
+      Customer = "${local.customer}"
+
+  }
+}
