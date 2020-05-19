1eb5b950baa11b55b19436a53db4b4cc1b5322a7
Methods <methods@hotmail.com>
2020-05-19 14:53:42 -0700

    fixing syntax

--- modules/iam/iam.tf
+++ modules/iam/iam.tf
@@ -18,7 +18,7 @@ resource "aws_iam_role" "orbis_roles" {
 }
 
 resource "aws_iam_policy_attachment" "policies" {
-  count      = length(var.iam_policy_arns)
+  count      = length(var.policy_arns)
   policy_arn = var.policy_arns[count.index]
   role       = aws_iam_role.orbis_roles.name
 }
