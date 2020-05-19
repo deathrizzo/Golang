40f39d4aa8333ac3eaeb7fbe96dfd1cc8d74fe6a
Methods <methods@hotmail.com>
2020-05-19 14:37:08 -0700

    formating these damn things

--- modules/iam/iam.tf
+++ modules/iam/iam.tf
@@ -4,21 +4,21 @@ data "aws_iam_policy_document" "instance-assume-role-policy" {
 
     principals {
       identifiers = var.service_ids
-      type = "Service"
+      type        = "Service"
     }
   }
 }
 
 resource "aws_iam_role" "orbis_roles" {
-  count = length(var.service_names)
-  name = var.service_names[count.index]
+  count              = length(var.service_names)
+  name               = var.service_names[count.index]
   assume_role_policy = data.aws_iam_policy_document.instance-assume-role-policy.json
-  path = var.path
-  tags = var.tags
+  path               = var.path
+  tags               = var.tags
 }
 
 resource "aws_iam_policy_attachment" "policies" {
-  count = length(var.iam_policy_arns)
+  count      = length(var.iam_policy_arns)
   policy_arn = var.policy_arns[count.index]
-  role = aws_iam_role.orbis_roles.name
+  role       = aws_iam_role.orbis_roles.name
 }
--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -1,40 +1,40 @@
 variable "service_names" {
   description = "service names to role"
-  type = list(string)
-  default = null
+  type        = list(string)
+  default     = null
 }
 
 variable "name" {
   description = "The name of the role."
-  type = string
-  default = null
+  type        = string
+  default     = null
 }
 
 variable "service_ids" {
   description = "List of identifiers for principals. e.g. ec2.amazonaws.com."
-  type = list(string)
+  type        = list(string)
 }
 
 variable "iam_policy_arns" {
   description = "List of IAM policy ARNs."
-  type = list(string)
-  default = []
+  type        = list(string)
+  default     = []
 }
 
 variable "path" {
   description = "The path to the role."
-  type = string
-  default = null
+  type        = string
+  default     = null
 }
 
 variable "description" {
   description = "The description of the role."
-  type = string
-  default = null
+  type        = string
+  default     = null
 }
 
 variable "tags" {
   description = "Key-value mapping of tags for the IAM role."
-  type = map(string)
-  default = {}
+  type        = map(string)
+  default     = {}
 }
