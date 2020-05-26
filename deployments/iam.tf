d1d9b4b5b2ed084d06f5415ab06413d54554b16b
Methods <methods@hotmail.com>
2020-05-26 12:39:58 -0700

    removing un-needed files

--- deployments/iam.tf
+++ deployments/iam.tf
@@ -1,30 +1,13 @@
-variable "service_names" {
-  description = "Create Iam roles with these names"
-  default     = ["neo", "trinity", "morpheus"]
-}
-
-data "aws_iam_policy_document" "instance-assume-role-policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam::595072229124:role/elzwhere"]
-    }
-  }
-}
-
-resource "aws_iam_role" "example" {
-  count              = length(var.service_names)
-  name               = var.service_names[count.index]
-  assume_role_policy = data.aws_iam_policy_document.instance-assume-role-policy.json
-  path               = "/orbis/"
-
+module "service_roles" {
+  source        = "../modules/iam"
+  service_names = ["ogm", "obm", "oam", "oxm", "ocm", "oem", "epg"]
+  service_ids   = ["arn:aws:iam::595072229124:role/elzwhere"]
+  path          = "/orbis/"
+  description   = "service roles"
+  policy_arns   = ["arn:aws:iam::aws:policy/AmazonS3FullAccess", "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"]
   tags = {
-    Environment = "${local.environment}"
-    Application = "${local.application}"
-    Team        = "${local.team}"
-    Customer    = "${local.customer}"
-
+    Environment = "dev"
+    Customer    = "dtc"
+    Team        = "dtc"
   }
 }
